9d7810fe29bef8b4523c39d1d051e90960f4be17
SonTH1 <sonksnb1122@gmail.com>
2022-08-15 00:17:42 +0700

    Reup

--- bot_play_stone.cpp
+++ bot_play_stone.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 #include <stdlib.h>
 
 using namespace std;
 
 void bot_play_stone(vector<vector<string>> &board)
 {
-    string temp = "";
+    string *str[100];
     int Count = 0;
-    do{
-        int i = rand()% board.size();
-        int j = rand()% board.size();
-        temp = board[i][j];
-        if( temp == "O" || temp == "o" ) {}
-        else{
-            board[i][j] = "O";
-            Count++;
+    for (int i = 0; i < board.size(); i++){
+        for(int j = 0; j < board[i].size(); j++){
+            if(board[i][j] != "O" && board[i][j] != "o"){
+                str[Count] = &board[i][j];
+                Count++;
+            }
         }
-    }while(Count != 1);
+    }
+    srand(time(NULL));
+    int temp = rand() % (Count - 0 + 1) + 0;
+    *str[temp] = "O";
 }
 
 int main()
