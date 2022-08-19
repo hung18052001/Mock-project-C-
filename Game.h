3a434f93777748d2abc7b4625b9666bd0c8d0cc8
MinhDuc21 <61420421+MinhDuc21@users.noreply.github.com>
2022-08-19 09:27:00 +0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -4,23 +4,32 @@
 #include "Cell.h"
 #include "Person.h"
 class Game {
-	private:
+	public:
+		Person *character1, *character2; //character1 luon di truoc va danh x
 		int size;
 		vector<vector<Cell>> board;
 		int current_x; // vi tri cua nuoc co vua duoc danh
 		int current_y;
 		Person *winner;
-	public: 
+	
 		Game();
-		Game(int size, vector<vector<Cell>> board, int current_x, int current_y, Person* winner);
+		Game(int size, Person *character1, Person *character2);
 		Game(int size);
-		Game(Game &game);
+		Game(const Game& game);
 		~Game();
-		bool check_win(); // check 3 o tiep
+		int get_x() const; //
+		int get_y() const;
+		void set_x(int x);      
+		void set_y(int y);
+		int check_win(); // if equal -1, mean game does not finish, if 0, game ties, else game has 1 winner
 		void add_stone(int x, int y);
 		// board[i][j].set_value('x');
 		void render(); //
-		
+		void finish_game(); // thuc hien cac hanh dong set up lai truoc khi game ket thuc
+		void set_winner();
+		void print_data();
+	   	
 		
 };
+
 #endif
