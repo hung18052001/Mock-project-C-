d2585f7b1345950d0f9166c093f4d45b67fda555
MinhDuc21 <61420421+MinhDuc21@users.noreply.github.com>
2022-08-19 09:24:02 +0700

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -16,4 +16,57 @@ class Cell {
 		}
 };
 
+class _Point{
+    int _x;
+    int _y;
+    // Tọa độ x y trên bàn cờ
+	int _check;
+	// Biến nhận biết X và O ( -1 = X , 1 = O, 0 = Ô trống )
+public:
+	int getX() {
+		return _x;
+	}
+
+	int getY() {
+		return _y;
+	}
+
+	int getCheck() {
+		return _check;
+	}
+
+	void setX(int pX) {
+		_x = pX;
+	}
+
+	void setY(int pY) {
+		_y = pY;
+	}
+
+	void setCheck(int value) {
+		_check = value;
+	}
+
+	_Point() {
+		_x = 0;
+		_y = 0;
+		_check = 0;
+	}
+
+	_Point(int x, int y) {
+		_x = x;
+		_y = y;
+		_check = 0;
+	}
+
+	_Point operator=(const _Point& p) {
+		_x = p._x;
+		_y = p._y;
+		_check = p._check;
+		return *this;
+	}
+
+	~_Point() {}
+};
+
 #endif
